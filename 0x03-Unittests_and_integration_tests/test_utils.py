3ceefc2d3d2d6583f7d1dd6ee2f014aff4a987e0
Bolexzy <boluoyewumi79@gmail.com>
2023-08-01 01:08:01 +0100

    Update: test that a KeyError is raised for the following inputs
    nested_map={}, path=("a",)
    nested_map={"a": 1}, path=("a", "b")

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -21,3 +21,14 @@ class TestAccessNestedMap(unittest.TestCase):
         '''
         result = access_nested_map(nested_map, path)
         self.assertEqual(result, expected_output)
+
+    @parameterized.expand([
+        ({}, ["a",], 'a'),
+        ({"a": 1}, ["a", "b"], 'b')
+        ])
+    def test_access_nested_map_exception(self, nested_map, path, key_error):
+        ''' Test method raises correct exception
+        '''
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(nested_map, path)
+            self.assertEqual(key_error, e.exception)
