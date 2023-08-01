c6195b86c9bbb05c2aa22067b922849f3a13976e
Bolexzy <boluoyewumi79@gmail.com>
2023-08-01 01:33:30 +0100

    Update:  implement the TestGetJson.test_get_json method

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,6 +5,8 @@
 import unittest
 from utils import access_nested_map
 from parameterized import parameterized
+import requests
+from unittest.mock import patch, Mock
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -32,3 +34,24 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as e:
             access_nested_map(nested_map, path)
             self.assertEqual(key_error, e.exception)
+
+
+class TestGetJson(unittest.TestCase):
+    ''' Class for testing get_json function
+    '''
+
+    @parametrized.expand([
+          ("http://example.com", {"payload": True}),
+          ("http://holberton.io", {"payload": False})
+      ])
+    def test_get_json(self, test_url, test_payload):
+        ''' Test method returns correct output, test_payload.
+        '''
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+
+        with patch('requests.get', return_value=mock_response):
+            result = get_json(test_url)
+            self.assertEqual(result, test_payload)
+            # check that mocked method called once per input
+            mock_response.json.assert_called_once()
